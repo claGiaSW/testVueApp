f33ae143be3fb7118f19f0ac4f1dd81128aac659
claGiaSW <claudia.giancola@softwire.com>
2024-11-20 11:42:14 +0000

    updated README and added "View all jobs" section to JobListings

--- README.md
+++ README.md
@@ -1,24 +1,17 @@
 # testvueapp
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
+Job Listing Vue App created following the "Vue.js Crash Course 2024" video from Traversy Media (https://www.youtube.com/watch?v=VeNfHj6MhgA).
 
 ## Project Setup
 
 ```sh
 npm install
 ```
+
+Other dependencies to download for the app to work:
+- axios
+- tailwind
+
 
 ### Compile and Hot-Reload for Development
 
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,9 +1,18 @@
 <script setup>
 import jobData from "@/jobs.json";
 import JobListing from "./JobListing.vue";
-import { ref } from "vue";
+import { ref, defineProps } from "vue";
+
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false
+    }
+});
 
 const jobs = ref(jobData);
+
 </script>
 
 <template>
@@ -12,7 +21,14 @@ const jobs = ref(jobData);
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <JobListing v-for="job in jobData" :key="job.id" :job="job"/>
+            <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job"/>
         </div>
+    </section>
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
     </section>
 </template>
