106ab5ec62869ecd36c91f392f9ead3b2b27df03
claGiaSW <claudia.giancola@softwire.com>
2024-11-20 11:18:23 +0000

    adding components for top section, cards and jobs

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps({
+       background: {
+        type: String,
+        default: "bg-grey-100"
+       } 
+    })
+</script>
+
+<template>
+    <div :class="`${background} p-6 rounded-lg shadow-md`">
+        <slot></slot>
+    </div>
+</template>
